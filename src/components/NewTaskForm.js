b2fb5bb77362829c331747bae04ebdabfd8bf346
Billy Dunn <bdunn27@gmail.com>
2024-04-10 23:55:24 -0400

    mini project

--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,16 +1,43 @@
-import React from "react";
+import { useState } from "react";
 
-function NewTaskForm() {
+function NewTaskForm({ categories, onTaskFormSubmit }) {
+  const initialData = { text: "", category: "Code" };
+  const [newTask, setNewTask] = useState(initialData);
+
+  const handleChange = (e) => {
+    setNewTask({ ...newTask, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // setTask((tasks) => [...tasks, newTask]);
+    onTaskFormSubmit(newTask);
+    console.log(newTask);
+    setNewTask(initialData);
+  };
   return (
-    <form className="new-task-form">
+    <form onSubmit={handleSubmit} className="new-task-form">
       <label>
         Details
-        <input type="text" name="text" />
+        <input
+          onChange={handleChange}
+          value={newTask.text}
+          type="text"
+          name="text"
+        />
       </label>
       <label>
         Category
-        <select name="category">
-          {/* render <option> elements for each category here */}
+        <select
+          onChange={handleChange}
+          value={newTask.category}
+          name="category"
+        >
+          {categories.map((category) => (
+            <option key={category} value={category}>
+              {category}
+            </option>
+          ))}
         </select>
       </label>
       <input type="submit" value="Add task" />
